e083aed757c7e769bf94c904bbed7893290758ba
zhuangpf <809566095@qq.com>
2024-01-07 10:51:06 +0800

    commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # devops
-devops
+
+
+
+
---
+++ software/arthas-install.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# ----------------------------------------------------------------------------------
+# 控制台颜色
+BLACK="\033[1;30m"
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+YELLOW="\033[1;33m"
+BLUE="\033[1;34m"
+PURPLE="\033[1;35m"
+CYAN="\033[1;36m"
+RESET="$(tput sgr0)"
+# ----------------------------------------------------------------------------------
+
+printf "${PURPLE}"
+cat << EOF
+# ----------------------------------------------------------------------------------
+# Arthas 安装脚本
+# @author: zhuangpf
+# ----------------------------------------------------------------------------------
+EOF
+printf "${RESET}"
+
+printf "${BLUE}>>>>>>>> begin.\n${RESET}"
+
+# 下载路径
+root=/opt/software/arthas
+if [[ -n $1 ]]; then
+	root=$1
+fi
+
+mkdir -p ${root}
+curl -o ${root}/arthas-boot.jar https://alibaba.github.io/arthas/arthas-boot.jar
+
+printf "${GREEN}[OK]\n${RESET}"
+printf "${BLUE}<<<<<<<< end.\n${RESET}"
